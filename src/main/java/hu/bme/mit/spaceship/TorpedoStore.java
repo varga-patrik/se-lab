34714266be9b99dca012e471b4d4aaf6e4534755
Varga Patrik <patrik040304@gmail.com>
2024-10-15 22:11:55 +0200

    typo fixed

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -39,7 +39,7 @@ public class TorpedoStore {
     // simulate random overheating of the launcher bay which prevents firing
     if (generator.nextDouble() >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
